5f33179d481460889805b1d5820e02f977ee487c
Serhii Stanislav <serhiistanislav@gmail.com>
2020-12-20 02:25:20 +0200

    adds markup

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,31 @@
-export default function fetchCountries(searchQuery) {
-   console.log(searchQuery)
+import countries from "../templates/countries.hbs";
+import targetCountrie from "../templates/target-country.hbs";
 
-   fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-   .then(response => response.json())
-   .then(data => console.log(data));
+const refs = {
+   countriesContainer: document.querySelector('.js-countries'),
 };
+
+const fetchCountries = searchQuery => {
+const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+fetch(url)
+.then(res => res.json())
+   .then(data => {
+      const markupCountries = countries(data);
+      const markupTargetCountry = targetCountrie(data);
+
+      markupHandler(data, markupCountries, markupTargetCountry);
+});
+};
+
+function markupHandler(value, countries, targetCountry) {
+   let markup = "";
+
+   value.length > 1 && value.length < 11
+      ? markup = countries
+      : markup = targetCountry;
+   
+   refs.countriesContainer.insertAdjacentHTML('beforeend', `${markup}`);
+}
+
+
+export default fetchCountries;
---
+++ src/js/notifications.js
@@ -0,0 +1,3 @@
+import { error } from '@pnotify/core/';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
